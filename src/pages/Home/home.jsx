01454f6d0ce516fe2729661704bf8f3eb2b55937
fullStack-devmax <heymywork01@gmail.com>
2025-04-12 14:57:01 +0500

    categories: price range and size dyna

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -18,16 +18,28 @@ export const Home = () => {
                         <aside className={home_style.ProductSorts_categories}>
                             <h3>Categories</h3>
                             <ul>
-                                <li><h4>House Plants</h4> <span>12</span></li>
-                                <li><h4>Potter Plants</h4> <span>12</span></li>
-                                <li><h4>Seeds</h4> <span>12</span></li>
-                                <li><h4>Small Plants</h4> <span>12</span></li>
-                                <li><h4>Big Plants</h4> <span>12</span></li>
-                                <li><h4>Succulents</h4> <span>12</span></li>
-                                <li><h4>Trerrariums</h4> <span>12</span></li>
-                                <li><h4>Gardening</h4> <span>12</span></li>
-                                <li><h4>Accessories</h4> <span>12</span></li>
+                                <li><h4>House Plants</h4> <span>&#40;12&#41;</span></li>
+                                <li><h4>Potter Plants</h4> <span>&#40;12&#41;</span></li>
+                                <li><h4>Seeds</h4> <span>&#40;12&#41;</span></li>
+                                <li><h4>Small Plants</h4> <span>&#40;12&#41;</span></li>
+                                <li><h4>Big Plants</h4> <span>&#40;12&#41;</span></li>
+                                <li><h4>Succulents</h4> <span>&#40;12&#41;</span></li>
+                                <li><h4>Trerrariums</h4> <span>&#40;12&#41;</span></li>
+                                <li><h4>Gardening</h4> <span>&#40;12&#41;</span></li>
+                                <li><h4>Accessories</h4> <span>&#40;12&#41;</span></li>
                             </ul>
+                            <h3 style={{paddingTop: '30px'}}>Price Range</h3>
+                            <div style={{paddingLeft: '5px'}}>
+                                <input type="range" />
+                                <span style={{padding: '15px 0'}}>Price: <p>$35-$165</p></span>
+                                <button>Filter</button>
+                            </div>
+                            <h3 style={{paddingTop: '30px'}}>Size</h3>
+                                <ul>
+                                    <li><h4>Small</h4> <span>&#40;119&#41;</span></li>
+                                    <li><h4>Medium</h4> <span>&#40;119&#41;</span></li>
+                                    <li><h4>Large</h4> <span>&#40;119&#41;</span></li>
+                                </ul>
                         </aside>
                         <div className={home_style.ProductSorts_pos}>
                             <nav className={home_style.ProductSorts_pos__sorting}>
